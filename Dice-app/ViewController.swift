8819831a5d32c34eb824953eb9a31511d0d49bd5
Registray <72702954+Registray@users.noreply.github.com>
2022-07-05 14:37:43 +0300

    Initial Commit

---
+++ Dice-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dice-app
+//
+//  Created by Oleh Pauk on 05.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
